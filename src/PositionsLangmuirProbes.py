regular backup, flux density calculation fixed to include times sin(zeta), not tested yet

src/PositionsLangmuirProbes.py
@@ -3,7 +3,12 @@
     In OP1.2 the Langmuir Probes are located in divertor module 5 on the horizontal target elements 3 and 4 of upper and lower divertor unit 
     In OP2 they are located in divertor module 5 on the horizontal target modules 2h07, 3h01, and 8h01 of upper and lower divertor unit
     Found in   https://wikis.ipp-hgw.mpg.de/W7X/index.php/QRP_-_Langmuir_Probe_Array_(Divertor),_OP1_flush_mounted,_OP2_pop-up
-               https://w7x-logbook.ipp-hgw.mpg.de/components?id=QRP02#'''
+               https://w7x-logbook.ipp-hgw.mpg.de/components?id=QRP02#
+    
+    Additionally, the local incident angle of the magnetic field lines on the target zeta is given at each langmuir probe position in [rad]
+    -> measured from the surface of the target towards the surface normal'''
+
+import numpy as np
 
 OP1_ProbeTM3 = ['TE3-01', 'TE3-02', 'TE3-03', 'TE3-04', 'TE3-05', 'TE3-06', 'TE3-07', 'TE3-08', 'TE3-09', 'TE3-10']
 OP1_ProbeTM4 = ['TE4-11', 'TE4-12', 'TE4-13', 'TE4-14', 'TE4-15', 'TE4-16', 'TE4-17', 'TE4-18', 'TE4-19', 'TE4-20']
@@ -18,3 +23,10 @@ OP2_ProbeTM8 = ['TE8-11', 'TE8-12', 'TE8-13', 'TE8-14', 'TE8-15', 'TE8-16', 'TE8
 OP2_TM2Distances = [0.10602897, 0.13182066, 0.15761507, 0.18341105, 0.20920802, 0.23500566] #Are those the right six values as TM2h07 is said to have Langmuir Probes closer to pumping gap?
 OP2_TM3Distances = [0.32530727, 0.35110524, 0.37690349, 0.40270196, 0.42850063, 0.45429944, 0.48009839, 0.50589745] #Are those the right eight values or are they messed up with TM2h07?
 OP2_TM8Distances = [0.09177, 0.11728, 0.13278, 0.15828] 
+
+OP1_TM3zeta = [np.deg2rad(2.)] * len(OP1_ProbeTM3)
+OP1_TM4zeta = [np.deg2rad(2.)] * len(OP1_ProbeTM4)
+
+OP2_TM2zeta = [np.deg2rad(2.)] * len(OP2_TM2Distances)
+OP2_TM3zeta = [np.deg2rad(2.)] * len(OP2_TM3Distances)
+OP2_TM8zeta = [np.deg2rad(2.)] * len(OP2_TM8Distances)

src/ReadArchieveDB.py
@@ -16,23 +16,27 @@ from src.heatflux_T import heatflux_T_download
 def getRuntimePerConfiguration(configurations: list[str] =['EIM000-2520', 'EIM000-2620', 'KJM008-2520', 'KJM008-2620', 'FTM000-2620', 'FTM004-2520', 'DBM000-2520', 'FMM002-2520',
                                                            'EIM000+2520', 'EIM000+2620', 'EIM000+2614', 'DBM000+2520', 'KJM008+2520', 'KJM008+2620', 'XIM001+2485', 'MMG000+2520', 
                                                            'DKJ000+2520', 'IKJ000+2520', 'FMM002+2520', 'KTM000+2520', 'FTM004+2520', 'FTM004+2585', 'FTM000+2620', 'AIM000+2520', 'KOF000+2520'],
-                               dischargeIDcsv: str ='results/configurations/dischargeList_OP223_', 
-                               safe: str ='results/configurations/configurationRuntimes.csv') -> None:
+                               campaign: str ='',
+                               dischargeIDcsv: str ='results/configurations/dischargeList_', 
+                               safe: str ='results/configurations/configurationRuntimes') -> None:
     ''' This function determines the absolute and relative runtime of each configuration over all discharges given by .csv files saved under "dischargeIDcsv" + "*", the results are written to .csv file saved under "safe"
         Each configuration has its own file, so function iterates over those files and sums up the number of discharges and their durations
-        The DataFrame from each file contains all discharges of this configuration and has the keys 'dischargeID', 'duration', 'configuration', and 'overviewTables' (default safe for calculation tables of a discharge)'''
+        The DataFrame from each file contains all discharges of this configuration and has the keys 'dischargeID', 'duration', 'configuration', and 'overviewTables' (default safe for calculation tables of a discharge)
+        "campaign" determines the OP being investigated (either 'OP22', 'OP23', or '' for both of them)'''
 
     runtimes = pd.DataFrame({})
     absoluteRuntimes, relativeRuntimes, absoluteNumberOfDischarges, relativeNumberOfDischarges = [], [], [], []
     totalRuntime = 0
     totalNumber = 0
 
+    if campaign == '':
+        campaign = 'OP223'
     for configuration in configurations:
-        if not os.path.isfile(dischargeIDcsv + configuration + '.csv'): #if there is no file, no discharge were found
+        if not os.path.isfile(dischargeIDcsv + campaign + '_' + configuration + '.csv'): #if there is no file, no discharge were found
             absoluteRuntimes.append(0)
             absoluteNumberOfDischarges.append(0)
         else:
-            dischargeList = pd.read_csv(dischargeIDcsv + configuration + '.csv', sep=';')
+            dischargeList = pd.read_csv(dischargeIDcsv + campaign + '_' + configuration + '.csv', sep=';')
             absoluteRuntimes.append(np.nansum(dischargeList['duration']))
             absoluteNumberOfDischarges.append(len(dischargeList['dischargeID']))
         totalRuntime += absoluteRuntimes[-1]
@@ -56,7 +60,7 @@ def getRuntimePerConfiguration(configurations: list[str] =['EIM000-2520', 'EIM00
     runtimes['relative runtime (%)'] = relativeRuntimes
     runtimes = runtimes.sort_values('absolute runtime (s)')
 
-    runtimes.to_csv(safe, sep=';')
+    runtimes.to_csv(safe + campaign + '.csv', sep=';')
     configurations.remove('all')
 
 #######################################################################################################################################################################
@@ -184,7 +188,7 @@ def readSurfaceTemperatureFramesFromIRcam(dischargeID: str,
         return [np.array(T_data[0]).T, np.array(T_data[1]).T, comment]
     else:
         return [np.array(T_data[0]).T, comment]
-    
+
 #######################################################################################################################################################################
 def readLangmuirProbeDataFromXdrive(dischargeID: str) -> str|list[list]:
     ''' This functions reads the electron density and electron temperature measured by the pop-up Langmuir Probes in "dischargeID" from xdrive
@@ -263,12 +267,15 @@ def readLangmuirProbeDataFromXdrive(dischargeID: str) -> str|list[list]:
 #######################################################################################################################################################################        
 def readAllShotNumbersFromLogbook(config: str, 
                                   filterSelected: str, 
+                                  q_add: str,
                                   filesExist: bool =False, 
-                                  safe: str ='results/configurations/dischargeList_OP223_', 
+                                  safe: str ='results/configurations/dischargeList', 
                                   overviewTableLink: str ='results/calculationTables/results_') -> pd.DataFrame|str:
     ''' This function is responsible for finding all discharges in the Logbook according to the given filter in OP2.2 and OP2.3
         "config" applies internal configuration filter
         "filterSelected" is externally given filter (no conditioning, no gas valve tests, no sniffer tests,...)
+        "q_add" is additional time filter for the year in string format
+        -> 'OP22' means 2024, 'OP23' means 2025, and '' means 2024-2025
         "filesExist" determines, if possibly existing files with discharges for that configuration are overwritten or read out and returned
         "safe" is the basic structure of the path where the created .csv file is saved
         "overviewTableLink" is the basic structure of the path where latter calculation tables for a discharge are going to be safed
@@ -278,11 +285,13 @@ def readAllShotNumbersFromLogbook(config: str,
         keys: "configuration", "dischargeID", "duration", "duration_planned", "durationHeating", and default safe for calculation tables "overviewTable"
         "duration" is time difference between trigger t1 and t4, "duration_planned" is planned ECRH duration, "durationHeating" is summed duration of ECRH and NBI
         DataFrame is saved as .csv file under modified "safe"'''
+    if q_add == '':
+        q_add = 'OP223'
     
     if filesExist:  #existing files are read out, missing ones are created
-        if os.path.isfile(safe + config + '.csv'):
+        if os.path.isfile(safe + '_' + q_add + '_' + config + '.csv'):
             print('confiuration discharge file exists and is read out')
-            return pd.read_csv(safe + config + '.csv', sep=';')
+            return pd.read_csv(safe + '_' + q_add + '_' + config + '.csv', sep=';')
         
     else:           #existing files are overwritted, missing ones are created
         pass
@@ -296,7 +305,13 @@ def readAllShotNumbersFromLogbook(config: str,
 
     #combine neccessary filters   
     q = filterSelected + q7 
-    p = {'time':'[2024 TO 2025]', 'size':'9999', 'q' : q }   # OP2.2 and OP2.3
+    if q_add == 'OP22':
+        p = {'time':'[2024 TO 2024]', 'size':'9999', 'q' : q }   # OP2.2
+    elif q_add == 'OP23':
+        p = {'time':'[2025 TO 2025]', 'size':'9999', 'q' : q }   # OP2.3
+    elif q_add == 'OP223':
+        q_add = 'OP223'
+        p = {'time':'[2024 TO 2025]', 'size':'9999', 'q' : q }   # OP2.2 and OP2.3
 
     dischargeIDs, duration_Trigger, duration_planned, duration_ECRH_NBI, overviewTables = [], [], [], [], []    
 
@@ -419,7 +434,7 @@ def readAllShotNumbersFromLogbook(config: str,
     configuration = [config] * len(dischargeIDs)
 
     dischargeTable = pd.DataFrame({'configuration': configuration, 'dischargeID': dischargeIDs, 'duration': duration_Trigger, 'duration_planned': duration_planned, 'durationHeating': duration_ECRH_NBI, 'overviewTable': overviewTables})
-    dischargeTable.to_csv(safe + config + '.csv', sep=';')
+    dischargeTable.to_csv(safe + '_' + q_add + '_' + config + '.csv', sep=';')
     
     return dischargeTable
 

src/SputteringYieldFunctions.py
@@ -32,9 +32,9 @@ if __name__ == '__main__':
 
     #Parameters for net erosion specifically for divertor
     lambda_nr, lambda_nl = 1, -1     #nonsense values, just signs are correct
-
 else:
-    from Application import e, u, k_B, k, E_TF, E_s, Q_y_chem, C_d_chem, E_thd_chem, E_ths_chem, E_th_chem, lambda_nr, lambda_nl
+    from src.settings import e, k_B, k, E_TF, E_s, Q_y_chem, C_d_chem, E_thd_chem, E_ths_chem, E_th_chem, lambda_nr, lambda_nl
+
 #######################################################################################################################################################################
 #######################################################################################################################################################################
 #Calculation of the Physical Sputtering Yield of Graphite under Bombardement with Hydrogen, Deuterium, Tritium , Helium, and Carbon Particles (self-sputtering)
@@ -353,19 +353,21 @@ def calculateTotalErosionYield(incidentParticle: str, T_i: int|float, targetMate
 #######################################################################################################################################################################
 #Calculation of Fluxes of Incident Ions and Eroded Particles, and Thickness of Eroded Layer according to Ref. 1
 
-def calculateFluxIncidentIonFromSpeed(c_w: int|float, n_e: int|float) -> float:
-    ''' This function calculates the incident ion flux density from ion velocity "c_w" in [m/s] and the electron density "n_e" in [1/m^3]
+def calculateFluxIncidentIonFromSpeed(c_w: int|float, n_e: int|float, zeta: int|float) -> float:
+    ''' This function calculates the incident ion flux density from ion velocity "c_w" in [m/s] and the electron density "n_e" in [1/m^3] 
+        "zeta" is the incident angle of the magnetig field lines on the target measured form the surface towards the surface normal
         Returns single value for flux density in [1/s*m^2]
         muss da noch * f mit rein für einzelne Ionenflüsse?'''
-    return c_w * n_e  
+    return c_w * n_e * np.sin(zeta)
 
 #######################################################################################################################################################################
-def calculateFluxIncidentIon(T_e: int|float, T_i: int|float, m_i: int|float, n_e: int|float, f: int|float) -> float:
+def calculateFluxIncidentIon(zeta: int|float, T_e: int|float, T_i: int|float, m_i: int|float, n_e: int|float, f: int|float) -> float:
     ''' This function calculates the incident ion flux density from electron and ion temperature "T_e" and "T_i" in [K]
         ion mass m_i in [kg], the electron density at last closed flux surface "n_e" in [1/m^3], ion concentration "f"
         Boltzmann constant "k_B" in [J/K]
+        "zeta" is the incident angle of the magnetig field lines on the target measured form the surface towards the surface normal
         Returns single value for flux density in [1/s*m^2]'''
-    return np.sqrt(k_B * (T_e + T_i)/m_i) * n_e * f
+    return np.sqrt(k_B * (T_e + T_i)/m_i) * n_e * f * np.sin(zeta)
 
 #######################################################################################################################################################################
 def calculateErosionRate(Y: list, flux: list, n_target: int|float) -> float:                                                               

src/Unused.py
@@ -34,8 +34,8 @@ if __name__ == '__main__':
     #Parameters for net erosion specifically for divertor
     lambda_nr, lambda_nl = 1, -1     #nonsense values, just signs are correct
 
-#else:
-#    from Application import e, u, k_B, k, E_TF, E_s, Q_y_chem, C_d_chem, E_thd_chem, E_ths_chem, E_th_chem, lambda_nr, lambda_nl
+else:
+    from src.settings import e, u, k_B, k, E_TF, E_s, Q_y_chem, C_d_chem, E_thd_chem, E_ths_chem, E_th_chem, lambda_nr, lambda_nl
 
 #######################################################################################################################################################################
 def readArchieveDB_ne_ECRH(dischargesCSV: pd.DataFrame):
@@ -384,6 +384,7 @@ def processOP2Dataold(discharge: str,
                       t_upper: list[list[int|float]], 
                       alpha: int|float, 
                       LP_position: list[int|float], 
+                      LP_zeta: list[int|float],
                       m_i: list[int|float], 
                       f_i: list[int|float], 
                       ions: list[str], 
@@ -397,6 +398,7 @@ def processOP2Dataold(discharge: str,
         -> times are given in [s] from trigger t1 of the discharge
         Incident angle of the ions  "alpha" is given in [rad]
         The distance of each langmuir probe from the pumping gap is given in "LP_position" in [m], probe indices 0 - 5 are on TM2h07, 6 - 13 on TM3h01, and 14 - 17 on TM8h01
+        The incident angle of the magnetic field lines on the target is given at each langmuir probe position from the target surface towards the surface normal in "LP_zeta" in [rad], probe indices 0 - 5 are on TM2h07, 6 - 13 on TM3h01, and 14 - 17 on TM8h01
         The ion masses "m_i" are in [kg], the ion concentrations "f_i", and ion names "ions" should have the same length as "m_i"
         The Boltzmann constant "k" must be in [eV/K]
         The atomic target density "n_target" should be provided in [1/m^3]
@@ -431,7 +433,7 @@ def processOP2Dataold(discharge: str,
             timesteps = np.array(timesteps)
 
             #calculate sputtering yields for each ion species ([H, D, T, C, O]), erosion rates, layer thicknesses
-            return_erosion = process.calculateErosionRelatedQuantitiesOnePosition(T_e, T_i, T_s, n_e, timesteps, alpha, m_i, f_i, ions, k, n_target)
+            return_erosion = process.calculateErosionRelatedQuantitiesOnePosition(T_e, T_i, T_s, n_e, timesteps, alpha, LP_zeta[LP_position[position - 1]], m_i, f_i, ions, k, n_target)
             if type(return_erosion) == str:
                 print(return_erosion)
                 exit()